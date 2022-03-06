b042e37d194db3b2586a5c1ff60f7632aba064fb
chandukanike <chanduyadv45@gmail.com>
2022-03-06 14:17:16 +0530

    task done

---
+++ Emailverification/src/com/Emailverification.java
@@ -0,0 +1,30 @@
+package com;
+import java.util.*;
+import java.util.regex.*;public class Emailverification {
+
+		public static void main(String[] args) {
+
+		List<String> emails = new ArrayList<String>();
+		// valid email addresses emails.add("ganesh@.com"); emails.add("rocky.rick@example.com"); emails.add("manoj@example.me.org");
+		//invalid email addresses emails.add("king.example.com"); emails.add("pavan..bob@example.com"); emails.add("srinu@.example.com");
+
+		for (String value : emails) {
+		System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+		}
+		System.out.println("Enter any email address to check"); Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+		}
+		public static boolean isValidEmail(String email) { String regex = "^(.+)@(.+)$";
+		// initialize the pattern object
+		Pattern pattern = Pattern.compile(regex); Matcher matcher = pattern.matcher(email); return matcher.matches();
+		
+		}
+		
+		
+		
+	
+		
+	}
+
+
---
+++ Typecasting/Accessspecifier1/src/com/Accessspecifier1.java
@@ -0,0 +1,28 @@
+package com;
+
+import Package1.Access1;
+
+	public class Access3 extends Access1{
+		
+		
+		
+		public static void main(String[] args) {
+			 
+			
+			Access3 a = new Access3();
+			System.out.println(a.hours);
+			System.out.println(a.minutes);
+		}
+
+	}
+
+package com;
+
+public class Accessspecifier1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
